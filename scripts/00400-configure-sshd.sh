42e2e55e2239e8ad75c17349e8128ddbee33cc3a
BradChesney79 <bradchesney79@gmail.com>
2016-12-24 06:36:08 -0500

    locked myself out...

--- scripts/00400-configure-sshd.sh
+++ scripts/00400-configure-sshd.sh
@@ -9,7 +9,7 @@ cat <<EOF > /etc/ssh/sshd_config
 LoginGraceTime 30
 PermitRootLogin yes
 MaxStartups 3:50:10
-AllowUsers $USERID1001
+AllowUsers root $USERID1001
 KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
 EOF
 
