cc718956bdafae094c75666e2f3e6f6a9b711f79
BradChesney79 <bradchesney79@gmail.com>
2016-12-24 15:00:12 -0500

    dev tools is a phpinfo page for now

---
+++ scripts/01300-install-dev-tools.sh
@@ -0,0 +1,9 @@
+printf "\n\n##### Beginning 01300-install-dev-tools.sh\n\n" >> /root/report/build-report.txt
+
+printf "\n## PLACE PHPINFO FILE IN THE WEBROOT ###"
+
+if [ "$DEV" = "true" ]
+  then
+    
+    printf "<?php phpinfo(); ?>" > $WEBROOT
+  fi
