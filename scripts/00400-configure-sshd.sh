3bf57642764f6b774d4eea6c8de7573e2f388581
BradChesney79 <bradchesney79@gmail.com>
2016-12-24 06:14:59 -0500

    locked myself out...

--- scripts/00400-configure-sshd.sh
+++ scripts/00400-configure-sshd.sh
@@ -2,10 +2,12 @@ printf "\n\n##### Beginning 00400-configure-sshd.sh\n\n" >> /root/report/build-r
 
 printf "\n## CONFIGURE SSH TO USE SUPPLEMENTAL ALGORITHMS ###\n"
 
+#TODO Permit root login, no
+
 cat <<EOF > /etc/ssh/sshd_config
 # Additional encryption algorithms for connecting via SSH
 LoginGraceTime 30
-PermitRootLogin no
+PermitRootLogin yes
 MaxStartups 3:50:10
 AllowUsers $USERID1001
 KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1
