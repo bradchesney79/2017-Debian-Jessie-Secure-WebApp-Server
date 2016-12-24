5c0ce228c8b090ecf1c01576ff6502f07be503ae
BradChesney79 <bradchesney79@gmail.com>
2016-12-24 07:26:13 -0500

    removed duplicate packages

--- scripts/00700-install-nginx-and-php7-fpm.sh
+++ scripts/00700-install-nginx-and-php7-fpm.sh
@@ -2,7 +2,7 @@ printf "\n\n##### Beginning 00700-install-nginx-and-php7-fpm.sh\n\n" >> /root/re
 
 printf "\n## INSTALLING THE PILE OF NGINX & PHP PACKAGES###\n"
 
-apt-get install -y git curl imagemagick nginx php-all-dev php-pear php7.0-fpm php7.0-dev php7.0-bz2 php7.0-calendar php7.0-curl php7.0-gd php7.0-imagick php7.0-imap php7.0-intl php7.0-json php7.0-ldap php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-pdo php7.0-phar php7.0-pspell php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-zip
+apt-get install -y nginx php-all-dev php-pear php7.0-fpm php7.0-dev php7.0-bz2 php7.0-calendar php7.0-curl php7.0-gd php7.0-imagick php7.0-imap php7.0-intl php7.0-json php7.0-ldap php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-pdo php7.0-phar php7.0-pspell php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-zip
 
 systemctl stop nginx
 systemctl stop php7.0-fpm
