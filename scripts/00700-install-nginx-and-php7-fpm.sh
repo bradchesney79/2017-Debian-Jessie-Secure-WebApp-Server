25fe573b0a0006b3f6adf3c1d5fe70c8c08ce5a8
BradChesney79 <bradchesney79@gmail.com>
2016-12-27 21:08:04 -0500

    troubleshooting an install problem

--- scripts/00700-install-nginx-and-php7-fpm.sh
+++ scripts/00700-install-nginx-and-php7-fpm.sh
@@ -9,10 +9,14 @@ passwd -l $USER
 
 printf "\n## INSTALLING THE PILE OF NGINX & PHP PACKAGES###\n"
  
-apt-get install -y nginx nginx-common php7.0-fpm php7.0-common
+apt-get install -y nginx nginx-common php7.0-fpm php7.0-common php7.0-curl php7.0-gd php7.0-imagick php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-xml
 
-# php7.0-dev php7.0-bz2 php7.0-curl php7.0-gd php7.0-imagick php7.0-imap php7.0-intl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-xml php7.0-xmlrpc php7.0-zip
+ # currently under test
+ # php7.0-curl php7.0-gd php7.0-imagick php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-xml
 
+# php7.0-dev php7.0-bz2  php7.0-imap php7.0-intl php7.0-xmlrpc php7.0-zip
+
+# replaced by php7.0-common
 # php7.0-phar php7.0-calendar php7.0-pdo
 # php7.0-ldap  php7.0-pspell php7.0-tidy php-all-dev php-pear
 
