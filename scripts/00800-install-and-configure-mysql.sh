10c9e4fee7aabc5d0ad4c7e087894d0e9fd47a21
BradChesney79 <bradchesney79@gmail.com>
2017-01-11 11:38:28 -0500

    prablems

--- scripts/00800-install-and-configure-mysql.sh
+++ scripts/00800-install-and-configure-mysql.sh
@@ -34,6 +34,76 @@ mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "CREATE USER 'backup'@'localhost' 
 
 mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT SELECT, SHOW VIEW, RELOAD, REPLICATION CLIENT, EVENT, TRIGGER ON *.* TO 'backup'@'localhost';"
 
+printf "\n## START SETTING UP SUPPORTING SYSTEM THINGS\n\n"
+
+cd $PROJECTROOT
+
+# Create databases for the app and sentinel
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "CREATE DATABASE $PROJECTDB;"
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "CREATE DATABASE $SENTINELDB;"
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $PROJECTDB.* TO '$USERID1001'@'$PROJECTDBHOST' IDENTIFIED BY '$USERID1001PASSWORD' WITH GRANT OPTION;"
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$USERID1001'@'$SENTINELDBHOST' IDENTIFIED BY '$USERID1001PASSWORD' WITH GRANT OPTION;"
+
+
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $PROJECTDB.* TO '$DEFAULTSITEDBUSER'@'$PROJECTDBHOST' IDENTIFIED BY '$DEFAULTSITEDBPASSWORD';"
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$DEFAULTSITEDBUSER'@'$SENTINELDBHOST' IDENTIFIED BY '$DEFAULTSITEDBPASSWORD';"
+
+mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "FLUSH PRIVILEGES;"
+
+printf "\n## OUTPUT THE COMMANDS ###\n"
+printf "\n\n
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"CREATE DATABASE $PROJECTDB;\"
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"CREATE DATABASE $SENTINELDB;\"
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"GRANT ALL PRIVILEGES ON $PROJECTDB.* TO '$USERID1001'@'$PROJECTDBHOST' IDENTIFIED BY '$USERID1001PASSWORD' WITH GRANT OPTION;\"
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$USERID1001'@'$SENTINELDBHOST' IDENTIFIED BY '$USERID1001PASSWORD' WITH GRANT OPTION;\"
+
+
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"GRANT ALL PRIVILEGES ON $PROJECTDB.* TO '$DEFAULTSITEDBUSER'@'$PROJECTDBHOST' IDENTIFIED BY '$DEFAULTSITEDBPASSWORD';\"
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"GRANT ALL PRIVILEGES ON $SENTINELDB.* TO '$DEFAULTSITEDBUSER'@'$SENTINELDBHOST' IDENTIFIED BY '$DEFAULTSITEDBPASSWORD';\"
+
+mysql -u\"$DBROOTUSER\" -p\"$DBROOTPASSWORD\" <<< \"FLUSH PRIVILEGES;\"
+\n\n"
+
+# Fill the database with data structure goodness
+
+mkdir -p /root/sql
+
+cat <<EOF > /root/sql/schema.sql
+
+CREATE TABLE `$PROJECTDB.users` (
+  `userid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'This is the be all and end all id, this is what identifies our user in our system.',
+  `active` char(1) DEFAULT NULL,
+  `fname` char(50) DEFAULT NULL COMMENT 'This is the user''s first name, birth certificate/legal name stuff',
+  `nickname` char(50) DEFAULT NULL COMMENT 'This is the nickname that a user would prefer to use in place of a first name-- which our app will respect',
+  `lname` char(50) DEFAULT NULL COMMENT 'The last name of the user',
+  `password` char(255) DEFAULT NULL COMMENT 'This field stores the password hash particulars / salted hash',
+  `sessionStart` int(10) unsigned DEFAULT NULL COMMENT 'When the user initially ''logged in'' and had a successful authentication as a timestamp',
+  `sessionRenewal` int(10) unsigned DEFAULT NULL COMMENT 'This is an activity that extends the active sesion as a timestamp',
+  `lastModified` int(10) unsigned DEFAULT NULL COMMENT 'This field stores the last change for searching in the logs',
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table is what it is all about.';"
+
+
+CREATE TABLE `$PROJECTDB.phones` (
+  `phonesid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NULL COMMENT 'The id that ties the phone to a user',
+  `phoneType` enum('landline','mobile','multi-ring','fax','tdd-tty','other') DEFAULT NULL COMMENT 'What type of device or endpoint is this number representing',
+  `sms` char(1) DEFAULT NULL COMMENT 'Can this phone receive sms entries',
+  `title` char(80) DEFAULT NULL COMMENT 'What the phone is to display to the user',
+  `areaCode` smallint(6) DEFAULT NULL COMMENT 'The area code of the phone number',
+  `prefix` smallint(6) DEFAULT NULL COMMENT 'The phone number prefix',
+  `number` smallint(6) DEFAULT NULL COMMENT 'The four numbers that make a phone number individual',
+  `extention` char(80) DEFAULT NULL COMMENT 'Extension information about the phone number',
+  `lastModified` int(10) unsigned DEFAULT NULL COMMENT 'This field is used to track the last change in the logs',
+  PRIMARY KEY (`phonesid`),
+  CONSTRAINT `phones_userid`
+    FOREIGN KEY (`userid`)
+    REFERENCES `$PROJECTDB`.`user` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='the table with US format phone numbers';"
+EOF
+
+mysql -u"$DEFAULTSITEDBUSER" -p"$DEFAULTSITEDBPASSWORD" < /root/sql/schema.sql
+
 printf "\n## MAKE PRIVILEGES CHANGES LIVE ###\n"
 
 mysql -u"$DBROOTUSER" -p"$DBROOTPASSWORD" <<< "FLUSH PRIVILEGES;"
