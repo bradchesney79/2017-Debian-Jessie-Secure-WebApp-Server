81ab8dbc95103fd4238f6388e474efe6464aafdd
BradChesney79 <bradchesney79@gmail.com>
2017-01-16 13:22:37 -0500

    test data

--- scripts/00300-update-system.sh
+++ scripts/00300-update-system.sh
@@ -566,7 +566,7 @@ apt-get -y update
 printf "\n## UPGRADE THE SYSTEM\n\n"
 
 printf "ca-certs\n"
-apt-get -qqy --allow-downgrades --allow-remove-essential --allow-change-held-packages --with-new-pkgs dist-upgrade
+apt-get -qqy --force-yes --ignore-hold dist-upgrade
 printf "ca-certs done\n"
 
 #apt-get -y dist-upgrade
