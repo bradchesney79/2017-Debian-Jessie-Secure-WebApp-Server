13dc6731d2e25031ea8f2702746200f1c65c9b00
BradChesney79 <bradchesney79@gmail.com>
2017-05-24 10:25:30 -0400

    added nodejs installation

---
+++ scripts/01900-install-nodejs.sh
@@ -0,0 +1,24 @@
+printf "\n\n##### Beginning 01900-install-nodejs.sh\n\n" >> /root/report/build-report.txt
+
+
+# Install nvm -- this gives me the willies
+#ToDo: Rig up some kind of checksum checking logic here
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
+
+# Install nodejs (I install more than I need to prevent warning messages: iojs & argon)
+nvm install iojs
+nvm install argon
+
+# Most recent LTS release
+nvm install v6.10.3
+
+# Latest version
+nvm install v7.10.0
+
+# Set LTS version as active
+nvm use v6.10.3
+
+
+npm install -g npm
+
+
