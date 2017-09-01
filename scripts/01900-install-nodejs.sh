e519756b4c5420cf33caf6c3d2025eef30f1bf2d
BradChesney79 <bradchesney79@gmail.com>
2017-09-01 11:33:25 -0400

    install nodejs & nodejs management globally

--- scripts/01900-install-nodejs.sh
+++ scripts/01900-install-nodejs.sh
@@ -1,33 +1,36 @@
 printf "\n\n##### Beginning 01900-install-nodejs.sh\n\n" >> /root/report/build-report.txt
 
 
-# Install nvm -- this gives me the willies
-# ToDo: Rig up some kind of checksum checking logic here
-mkdir -p $PROJECTROOT/tmp/node
+## Install nvm -- this gives me the willies
+## ToDo: Rig up some kind of checksum checking logic here
+#mkdir -p ${PROJECTROOT}/tmp/node
+#
+#curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh > ${PROJECTROOT}/tmp/node/install.js
+#
+#chmod 700 ${PROJECTROOT}/tmp/node/install.js
+#
+#${PROJECTROOT}/tmp/node/install.js
+#
+## Load nvm
+## ToDo: Fix this, this isn't right-- this should be under the webuser
+#chmod 700 /root/.nvm/nvm.sh
+#. /root/.nvm/nvm.sh
+#
+## Install nodejs (I install more than I need to prevent warning messages: iojs & argon)
+#nvm install iojs
+#nvm install argon
+#
+## Most recent LTS release
+#nvm install v6.11.2
+#
+## Latest version
+#nvm install v8.4.0
+#
+## Set LTS version as active
+#nvm alias default v6.10.3
 
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh > $PROJECTROOT/tmp/node/install.js
-
-chmod 700 $PROJECTROOT/tmp/node/install.js
-
-/root/tmp/node/install.js
-
-# Load nvm
-# ToDo: Fix this, this isn't right-- this should be under the webuser
-chmod 700 /root/.nvm/nvm.sh
-. /root/.nvm/nvm.sh
-
-# Install nodejs (I install more than I need to prevent warning messages: iojs & argon)
-nvm install iojs
-nvm install argon
-
-# Most recent LTS release
-nvm install v6.11.2
-
-# Latest version
-nvm install v8.4.0
-
-# Set LTS version as active
-nvm alias default v6.10.3
+# install nodejs globally
+curl -L https://git.io/n-install | N_PREFIX=~/opt/n bash -s -- -y
 
 npm install -g npm
 
