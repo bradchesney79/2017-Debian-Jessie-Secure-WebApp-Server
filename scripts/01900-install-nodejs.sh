1966dfc89b29389782b7b5e4f5424e224454bb47
BradChesney79 <bradchesney79@gmail.com>
2017-09-01 18:53:25 -0400

    more nodejs work

--- scripts/01900-install-nodejs.sh
+++ scripts/01900-install-nodejs.sh
@@ -1,33 +1,9 @@
 printf "\n\n##### Beginning 01900-install-nodejs.sh\n\n" >> /root/report/build-report.txt
 
 
-## Install nvm -- this gives me the willies
-## ToDo: Rig up some kind of checksum checking logic here
-#mkdir -p ${PROJECTROOT}/tmp/node
-#
-#curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh > ${PROJECTROOT}/tmp/node/install.js
-#
-#chmod 700 ${PROJECTROOT}/tmp/node/install.js
-#
-#${PROJECTROOT}/tmp/node/install.js
-#
-## Load nvm
-## ToDo: Fix this, this isn't right-- this should be under the webuser
-#chmod 700 /root/.nvm/nvm.sh
-#. /root/.nvm/nvm.sh
-#
-## Install nodejs (I install more than I need to prevent warning messages: iojs & argon)
-#nvm install iojs
-#nvm install argon
-#
-## Most recent LTS release
-#nvm install v6.11.2
-#
-## Latest version
-#nvm install v8.4.0
-#
-## Set LTS version as active
-#nvm alias default v6.10.3
+## Install n -- this gives me the willies
+
+cd $PROJECTROOT
 
 # install nodejs
 curl -L https://git.io/n-install | N_PREFIX=~/opt/n bash -s -- -y
@@ -77,8 +53,7 @@ PATH=/opt/n/.npm-global/node/bin:$PATH
 
 . "${HOME}/.bashrc"
 
-# 777 works... let's try 775
-chmod -r 775 /opt/n
+chmod -r 777 /opt/n
 
 n stable # install this version, set to default, & switch to it
 
