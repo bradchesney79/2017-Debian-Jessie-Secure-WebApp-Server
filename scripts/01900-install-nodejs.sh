d52a7e12d27696c68a44c4accd148e0b4b8c3595
BradChesney79 <bradchesney79@gmail.com>
2017-09-01 12:50:22 -0400

    more nodejs work

--- scripts/01900-install-nodejs.sh
+++ scripts/01900-install-nodejs.sh
@@ -29,8 +29,42 @@ printf "\n\n##### Beginning 01900-install-nodejs.sh\n\n" >> /root/report/build-r
 ## Set LTS version as active
 #nvm alias default v6.10.3
 
-# install nodejs globally
-curl -L https://git.io/n-install | N_PREFIX=~/opt/n bash -s -- -y
+# prep for global nodejs install
+
+echo "NPM_CONFIG_PREFIX=/opt/n/.npm-global" /etc/environment
+export NPM_CONFIG_PREFIX=/opt/n/.npm-global
+
+# install nodejs
+curl -L https://git.io/n-install | N_PREFIX=~/opt bash -s -- -y
+
+cat <<EOF > $HOME/.bashrc
+# ~/.bashrc: executed by bash(1) for non-login shells.
+
+# Note: PS1 and umask are already set in /etc/profile. You should not
+# need this unless you want different defaults for root.
+# PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
+# umask 022
+
+# You may uncomment the following lines if you want `ls' to be colorized:
+# export LS_OPTIONS='--color=auto'
+# eval "`dircolors`"
+# alias ls='ls $LS_OPTIONS'
+# alias ll='ls $LS_OPTIONS -l'
+# alias l='ls $LS_OPTIONS -lA'
+#
+# Some more alias to avoid making mistakes:
+# alias rm='rm -i'
+# alias cp='cp -i'
+# alias mv='mv -i'
+
+export N_PREFIX="/opt/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+EOF
+
+mv $HOME/opt/n /opt/
+
+n stable # install this version, set to default, & switch to it
+
+n  -d lts # download only
 
 npm install -g npm
 
